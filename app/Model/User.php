adcadcd0eebac9502b9df4c34f8f7ad8bbe7fe09
zawmyohtut <zaw@intersective.com>
2015-06-01 16:43:24 +1000

    Documenation and commenting added to the code.

--- app/Model/User.php
+++ app/Model/User.php
@@ -1,16 +1,31 @@
 <?php 
+	
+/**
+* Application model for application.
+* 
+* User model is related to users table in the database due to the naming convention.
+* Users data used in this application is mananged and manipulated through this model.
+*
+*/
 
 	class User extends AppModel
-	{		
+	{	
+		/**
+		* @return boolean
+		*
+		* Method created for the purpse of hashing the password from user registering form.
+		* This method will be called before the User add form is submitted. 
+		*/
 		public function beforeSave($options = array()){
 
+			// created unhashed password for the future references.
 			$this->data['User']['unhashed_password'] = $this->data['User']['password'];
 			$this->data['User']['password'] = 
 			AuthComponent::password($this->data['User']['password']);			
 			return true;		
 		}	
 
-		
+		//validation made for the input of the user add page.
 		public $validate = array(
 
 			'last_name' => array(
@@ -96,6 +111,7 @@
 
 		);
 		
+		// method to check if two password input fields are the same.
 		public function validatePasswordConfirm(){
 
 			if($this->data['User']['password'] != $this->data['User']['confirm_password'])
--- app/View/Categories/add.ctp
+++ app/View/Categories/add.ctp
@@ -3,6 +3,6 @@
 <?php 
 	  echo $this->Form->create('Category');
 	  echo $this->Form->input('name');
-	  echo $this->Form->input('length_type');		  
+	  echo $this->Form->input('length_type',array('label' => 'Game play hours'));		  
 	  echo $this->Form->end('Save Category');
 ?>
